6cf9ffe9191aa6e0de82500f5ec36b566fe1427f
Adefolarin Osu <aosu@teamapt.com>
2020-07-30 14:17:03 +0100

    Refactor Services to common base interface. Closes #11

--- pet-clinic-data/src/main/java/com/phos/petclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/phos/petclinic/service/OwnerService.java
@@ -2,18 +2,8 @@ package com.phos.petclinic.service;
 
 import com.phos.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String name);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-
-
 
 }
--- pet-clinic-data/src/main/java/com/phos/petclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/phos/petclinic/service/VetService.java
@@ -2,13 +2,5 @@ package com.phos.petclinic.service;
 
 import com.phos.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
